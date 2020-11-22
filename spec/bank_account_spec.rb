a20beb612a986e036337bd1be0ff1365d1d4c6ef
04alexklink <alex_klink123@hotmail.co.uk>
2020-11-22 15:15:52 +0000

    Withdraw method created that takes a withdrawal amount as an argument and decreases bankaccount balance by this amount. Test passes

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -6,7 +6,15 @@ describe "BankAccount" do
   describe "#deposit" do
     it "increases bankaccount balance by 1000 when passed 1000 as an argument" do
       bankaccount.deposit(1000)
-      expect(bankaccount.show_balance).to eq(1000);
+      expect(bankaccount.show_balance).to eq(1000)
+    end
+  end
+  
+  describe "#withdraw" do 
+    it "decreases bankaccount balance by amount passed in as an argument" do
+      bankaccount.deposit(1000)
+      bankaccount.withdraw(500)
+      expect(bankaccount.show_balance).to eq(500)
     end
   end
 end
