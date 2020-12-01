48503305a2b45ba051b757075ce2ff3b58e0bb2d
04alexklink <alex_klink123@hotmail.co.uk>
2020-12-01 21:59:17 +0000

    Refactor: clarified statement_double variable name

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,21 +1,21 @@
 require 'bank_account'
 
 describe "BankAccount" do
-  let(:statement) { double :statement }
-  let(:bankaccount) { BankAccount.new(statement, transaction_class_double) }
+  let(:statement_double) { double :statement }
+  let(:bankaccount) { BankAccount.new(statement_double, transaction_class_double) }
   let(:transaction_double) { double :transaction}
   let(:transaction_class_double) { double :transaction_class, new: transaction_double }
   describe "#deposit" do
     it "knows that 1000 has been deposited " do
       expect(transaction_class_double).to receive(:new).with("23/11/2020", "deposit", 1000, 1000)
       bankaccount.deposit(1000, "23/11/2020")
-      allow(statement).to receive(:print_statement).with([transaction_double]).and_return("date || credit || debit || balance\n23/11/2020 || 1000.00 || || 1000.00 ")
+      allow(statement_double).to receive(:print_statement).with([transaction_double]).and_return("date || credit || debit || balance\n23/11/2020 || 1000.00 || || 1000.00 ")
       expect(bankaccount.print_statement).to eq("date || credit || debit || balance\n23/11/2020 || 1000.00 || || 1000.00 ")
     end
     it "knows that 500 has been deposited " do
       expect(transaction_class_double).to receive(:new).with("23/11/2020", "deposit", 500, 500)
       bankaccount.deposit(500, "23/11/2020")
-      allow(statement).to receive(:print_statement).with([transaction_double]).and_return("date || credit || debit || balance\n23/11/2020 || 500.00 || || 500.00 ")
+      allow(statement_double).to receive(:print_statement).with([transaction_double]).and_return("date || credit || debit || balance\n23/11/2020 || 500.00 || || 500.00 ")
       expect(bankaccount.print_statement).to eq("date || credit || debit || balance\n23/11/2020 || 500.00 || || 500.00 ")
     end
     it "does not accept negative numbers or string input for amount" do
@@ -29,7 +29,7 @@ describe "BankAccount" do
       bankaccount.deposit(1000, "23/11/2020")
       expect(transaction_class_double).to receive(:new).with("23/11/2020", "withdrawal", 500, 500)
       bankaccount.withdraw(500, "23/11/2020")
-      allow(statement).to receive(:print_statement).with([transaction_double, transaction_double]).and_return("date || credit || debit || balance\n23/11/2020 || || 500.00 || 500.00 \n23/11/2020 || 1000.00 || || 1000.00 ")
+      allow(statement_double).to receive(:print_statement).with([transaction_double, transaction_double]).and_return("date || credit || debit || balance\n23/11/2020 || || 500.00 || 500.00 \n23/11/2020 || 1000.00 || || 1000.00 ")
       expect(bankaccount.print_statement).to eq("date || credit || debit || balance\n23/11/2020 || || 500.00 || 500.00 \n23/11/2020 || 1000.00 || || 1000.00 ")
     end
     it "raises error message if withdrawal amount exceeds bankaccount balance" do
